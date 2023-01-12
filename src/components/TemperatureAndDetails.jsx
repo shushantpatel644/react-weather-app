5e61043110c79ba7e3d987f905d7806c2c538716
Shushant Patel <97536200+shushantpatel644@users.noreply.github.com>
2023-01-13 01:04:13 +0530

    React Weather App

---
+++ src/components/TemperatureAndDetails.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+    UilTemperature,
+    UilTear,
+    UilWind,
+    UilSun,
+    UilSunset,
+} from "@iconscout/react-unicons";
+import { formatToLocalTime, iconUrlFromCode } from "../services/weatherService";
+
+function TemperatureAndDetails() {
+    return <div>
+
+        <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+            <p>Cloudy or whatever</p>
+        </div>
+
+        <div className="flex felx-row items-center justify-between text-white py-3">
+            <img src="http://openweathermap.org/img/wn/01d@2x.png"
+                alt=""
+                className="w-20"
+            />
+            <p className="text-5xl">34</p>
+            <div className="flex flex-col space-y-2"></div>
+            <div className="flex font-light text-sm items-center justify-center"></div>
+        </div>
+    </div>
+}
+
+export default TemperatureAndDetails
