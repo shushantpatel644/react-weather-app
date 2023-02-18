90f236dd8dbfc6c86f0dd9d5b9abc8b887df5d05
Shushant Patel <97536200+shushantpatel644@users.noreply.github.com>
2023-02-18 22:03:57 +0530

    react-weather-app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Forecast from './components/Forecast';
 // import getWeatherData from './Services/weatherServices';
 import getFormattedWeatherData from './Services/weatherServices';
 import { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -18,8 +20,15 @@ function App() {
 
   useEffect(() => {
     const fetchWeather = async () => {
+      const message = query.q ? query.q : 'current locatio.'
+
+      toast.info('Fetching weather for' + message)
+
+
       await getFormattedWeatherData({ ...query, units }).then(
         (data) => {
+          toast.success(`Successfully fetched weather for ${data.name},${data.country}`);
+
           setWeather(data);
         });
     };
@@ -27,10 +36,17 @@ function App() {
     fetchWeather();
   }, [query, units]);
 
+  const formatBackground = () => {
+    if (!weather) return ' from-cyan-700 to-blue-700'
+    const threshold = units == 'metric' ? 20 : 60
+    if (weather.temp <= threshold) return 'from-cyan-700 to-blue-700'
+
+    return 'from-yellow-700 to-orange-700'
+  }
   return (
-    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
-      <TopButtons />
-      <Inputs />
+    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ${formatBackground}`}>
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
       {weather && (
         <div>
@@ -40,6 +56,8 @@ function App() {
           <Forecast title="daily forecast" items={weather.daily} />
         </div>
       )}
+
+      <ToastContainer autoClose={5000} theme="colored" newesOnTop={true} />
     </div>
   );
 }
